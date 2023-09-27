40911eba63d384aaed2d24d9de4a04d42921819e
pedrogb1 <86267784+pedrogb1@users.noreply.github.com>
2023-09-26 23:18:29 -0300

    Update Anvisa_scraper.py
    Finished Automating + More Data

--- Anvisa_scraper.py
+++ Anvisa_scraper.py
@@ -1,14 +1,11 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-# from webdriver_manager.chrome import ChromeDriverManager
-
-from copy import deepcopy
-import re
+from selenium.webdriver.common.by import By
+# from selenium.webdriver.chrome.service import Service
+# from selenium.webdriver.chrome.options import Options
 from time import sleep
 import pandas as pd
-# import requests
+import re
 
 url_anvisa = 'https://consultas.anvisa.gov.br/#'
 url_db = f'{url_anvisa}/bulario/q/'
@@ -16,9 +13,8 @@ lista_df = []
 
 def start_driver(url):
     # Config Driver
-    # option = webdriver.ChromeOptions()
+    options = webdriver.FirefoxOptions()
     # For headless mode, the 2 lines below are required
-    options = Options()
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--incognito')
@@ -27,31 +23,46 @@ def start_driver(url):
     # options.add_argument('start-maximized')
 
     # Path to chromedriver 
-    driver = webdriver.Chrome(service=Service(r'D:\pedro\Desktop\Challenge2023_SANOFI\chromedriver.exe'), options=options)
-
+    driver = webdriver.Firefox(options=options)
 
     driver.get(url)
-    sleep(3)
-    return driver
-
-
-
-
-def main():
-    driver = start_driver(url_db)
-    driver
-    
+    sleep(10)
     soup = BeautifulSoup(driver.page_source, 'html.parser')
 
+    #find button 50
+    element = driver.find_element(By.XPATH, '//*[@id="containerTable"]/div/div/div/div/button[3]')
+    element.click()
+    sleep(10)
+    return driver
+
+def nextPage(driver):
+    driver
+    element = driver.find_element(By.XPATH, '//*[@id="containerTable"]/div/div/div/ul/li[11]/a')
+    element.click()
+    sleep(10)
+    
+def recallPage(driver, page):
+    driver
+    driver.get(url_db)
+    sleep(10)
+    #find button 50
+    element = driver.find_element(By.XPATH, '//*[@id="containerTable"]/div/div/div/div/button[3]')
+    element.click()
+    sleep(10)
+    for temp_page in range(0, page):
+        element = driver.find_element(By.XPATH, '//*[@id="containerTable"]/div/div/div/ul/li[11]/a')
+        element.click()
+        sleep(10)
+
+
+
+def getPage(driver):
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
     names_compounds = soup.select("a.ng-binding")
     codes_compounds = [str((drug.get('href').split('/')[-2])) for drug in names_compounds[2:-1]]
     names_compounds = [drug.text for drug in names_compounds[2:-1]]
-    # [print(drug) for drug in names_compounds]
-    # [print(code) for code in codes_compounds]
-
     name_code = list(zip(names_compounds,codes_compounds))
-    # print(name_code)
-    
+
     for i in range(0,len(name_code)):
         code = name_code[i][1]
         url_med = f'{url_anvisa}/medicamentos/{code}/'
@@ -71,7 +82,6 @@ def main():
         # print(name_code[i][0])
         # [print('->', drug) for drug in sub_names_compounds]
         
-        # princ_ativo = deepcopy(name_code[i][0])
         for drug in sub_names_compounds:
             lista_df.append({
                 "Principio Ativo":[name_code[i][0]],
@@ -83,4 +93,31 @@ def main():
         lista_csv = pd.concat(lista_csv)
         print(lista_csv)
         lista_csv.to_csv('data_med.csv', index=False)
+
+def main():
+    driver = start_driver(url_db)
+    sleep(10)
+    
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+
+    names_compounds = soup.select("a.ng-binding")
+    codes_compounds = [str((drug.get('href').split('/')[-2])) for drug in names_compounds[2:-1]]
+    names_compounds = [drug.text for drug in names_compounds[2:-1]]
+    # [print(drug) for drug in names_compounds]
+    # [print(code) for code in codes_compounds]
+
+    name_code = list(zip(names_compounds,codes_compounds))
+    # print(name_code)
+    last_page = int(driver.find_element(By.XPATH, "/html/body/div[3]/div[1]/form/div/div[2]/div/div/div/ul/li[10]/a/span").text)
+
+    
+    for page in range(0,last_page - 1):
+        print("Page", page + 1, sep=" ")
+        getPage(driver)
+        print("Page", page + 1, "END", sep=" ")
+        print("-_"*100)
+        recallPage(driver, page)
+        nextPage(driver)
+    
+
 main()
